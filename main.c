386af3945418ef3b94907f51cd209202ac8ebbb5
hyunie47 <cfhyunie47@gmail.com>
2018-11-01 15:36:36 +0900

    initial submission

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	#define ROWS 3
+	#define COLS 3
+	
+	void printMatrix(int A[][COLS])
+	{
+		int i, j;
+		
+		for (i=0; i<ROWS; i++) {
+			for (j=0; j<COLS; j++) {
+				printf("%d", A[i][j]);
+			}
+			printf("\n");
+		}
+		
+		return;
+	 }
+	 int addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+	 {
+	 	int i, j;
+	 	
+	 	for (i=0; i<ROWS; i++) {
+	 		for (j=0; j<COLS; j++) {
+	 			C[i][j] = A[i][j] + B[i][j];
+			 }
+		 }
+		 return 1;
+	}}
